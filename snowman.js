ef268423f80d8937f238e378bb28e6fc6f76a902
JacobPetitdeMange <jakepdm@udel.edu>
2020-10-14 22:11:45 -0400

    final fixes

--- snowman.js
+++ snowman.js
@@ -15,7 +15,7 @@ firebase.initializeApp(firebaseConfig);
 let myDatabase = firebase.database();
 
 let answer = '';
-let answerLength = 0; 
+let answerLength = 0;
 let c1 = '';
 let c2 = '';
 let maxWrong = 8;
@@ -24,32 +24,32 @@ let guessed = [];
 let wordStatus = null;
 
 function randomWord() {
-        let wordcount = 224;
-        let randomword = Math.floor(wordcount * Math.random());
-      
-  myDatabase.ref("cword").child(randomword).once("value", ss=>{
-    answer = ss.val().toString();
-  });
- 
-            console.log("Answer is: " + answer);
-        }
+    let wordcount = 224;
+    let randomword = Math.floor(wordcount * Math.random());
 
-function fillLetters(){
-  
-   answerLength = answer.length;
-  c1 = String(answer).charAt(Math.floor(answerLength * Math.random()));
-  c2 = String(answer).charAt(Math.floor(answerLength * Math.random()));
-  guessed.push(c1);
-  guessed.push(c2);
-  
-  wordStatus = answer.split('').map(c1 => (guessed.indexOf(c1) >= 0 ? c1 : " _ ")).join('');
+    myDatabase.ref("cword").child(randomword).once("value", ss => {
+        answer = ss.val().toString();
+    });
 
-                document.getElementById('wordSpotlight').innerHTML = wordStatus;
+    console.log("Answer is: " + answer);
 }
 
-function generateButtons() {
-                let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz '.split('').map(letter =>
-                    `
+function fillLetters() {
+
+    answerLength = answer.length;
+    c1 = String(answer).charAt(Math.floor(answerLength * Math.random()));
+    c2 = String(answer).charAt(Math.floor(answerLength * Math.random()));
+    guessed.push(c1);
+    guessed.push(c2);
+
+    wordStatus = answer.split('').map(c1 => (guessed.indexOf(c1) >= 0 ? c1 : " _ ")).join('');
+
+    document.getElementById('wordSpotlight').innerHTML = wordStatus;
+}
+
+function makeButtons() {
+    let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz '.split('').map(letter =>
+        `
         <button
           class="btn btn-lg btn-dark m-2"
           id='` + letter + `'
@@ -59,68 +59,68 @@ function generateButtons() {
         </button>
       `).join('');
 
-                document.getElementById('keyboard').innerHTML = buttonsHTML;
-            }
+    document.getElementById('keyboard').innerHTML = buttonsHTML;
+}
 
-function handleGuess(chosenLetter) {
-                //guessed.push(c1);
-                //guessed.push(c2);
-                guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
-                document.getElementById(chosenLetter).setAttribute('disabled', true);
+function handleGuess(selection) {
+    //guessed.push(c1);
+    //guessed.push(c2);
+    guessed.indexOf(selection) === -1 ? guessed.push(selection) : null;
+    document.getElementById(selection).setAttribute('disabled', true);
 
-                if (answer.indexOf(chosenLetter) >= 0) {
-                    guessedWord();
-                    checkIfGameWon();
-                } else if (answer.indexOf(chosenLetter) === -1) {
-                    wrongCount++;
-                    updateWrongCount();
-                    checkIfGameLost();
-                    updateHangmanPicture();
-                }
-            }
+    if (answer.indexOf(selection) >= 0) {
+        playerGuess();
+        gameWon();
+    } else if (answer.indexOf(selection) === -1) {
+        wrongCount++;
+        updateWrongCount();
+        gameLost();
+        updateHangmanPicture();
+    }
+}
 
 function updateHangmanPicture() {
-                document.getElementById('hangmanPic').src = './images/' + wrongCount + '.jpg';
-            }
+    document.getElementById('hangmanPic').src = './images/' + wrongCount + '.jpg';
+}
 
-function checkIfGameWon() {
-                if (wordStatus === answer) {
-                    document.getElementById('keyboard').innerHTML = 'Game Over: You Win';
-                }
-            }
+function gameWon() {
+    if (wordStatus === answer) {
+        document.getElementById('keyboard').innerHTML = 'Game Over: You Win';
+    }
+}
 
-function checkIfGameLost() {
-                if (wrongCount === maxWrong) {
-                    document.getElementById('wordSpotlight').innerHTML = 'The answer was: ' + answer;
-                    document.getElementById('keyboard').innerHTML = 'Game Over: Snowman wins';
-                }
-            }
+function gameLost() {
+    if (wrongCount === maxWrong) {
+        document.getElementById('wordSpotlight').innerHTML = 'The correct word was: ' + answer;
+        document.getElementById('keyboard').innerHTML = 'Game Over: Snowman wins';
+    }
+}
 
-function guessedWord() {
-                wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
+function playerGuess() {
+    wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
 
-                document.getElementById('wordSpotlight').innerHTML = wordStatus;
-            }
+    document.getElementById('wordSpotlight').innerHTML = wordStatus;
+}
 
 function updateWrongCount() {
-                document.getElementById('wrongCount').innerHTML = wrongCount;
-            }
+    document.getElementById('wrongCount').innerHTML = wrongCount;
+}
 
 function reset() {
-                wrongCount = 0;
-                guessed = [];
-                document.getElementById('hangmanPic').src = './images/0.jpg';
+    wrongCount = 0;
+    guessed = [];
+    document.getElementById('snowmanImg').src = './images/0.jpg';
 
-                randomWord();
-                guessedWord();
-                updateWrongCount();
-                generateButtons();
-                //fillLetters();
-            }
+    randomWord();
+    playerGuess();
+    updateWrongCount();
+    makeButtons();
+    //fillLetters();
+}
 
 document.getElementById('maxWrong').innerHTML = maxWrong;
 
-        randomWord();
-        generateButtons();
-        guessedWord();
+randomWord();
+makeButtons();
+playerGuess();
         //fillLetters();
